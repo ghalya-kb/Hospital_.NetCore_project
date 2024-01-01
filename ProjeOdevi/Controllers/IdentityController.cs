96e145ab937a88fd1a06222e75802e73f1efc1fc
ghalya-kb <ghalyaqkq@gmail.com>
2024-01-01 22:46:27 +0300

    Kullanıcı kayıt işlemleri tamamlandı.

--- ProjeOdevi/Controllers/IdentityController.cs
+++ ProjeOdevi/Controllers/IdentityController.cs
@@ -21,7 +21,7 @@ namespace ProjeOdevi.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(LoginModel modelLogin)
+        public async Task<IActionResult> Login(Hasta modelLogin)
         {
             var kul = _context.Kuallanicilar.FirstOrDefault(x=>x.KullaniciAdi == modelLogin.KullaniciAdi && x.Sifre == modelLogin.Sifre);
 
@@ -59,6 +59,56 @@ namespace ProjeOdevi.Controllers
 
             ViewData["ValidateMessage"] = "user not found";
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(Hasta modelSignIn)
+        {
+            var userNameVarMi = _context.Kuallanicilar.Any(x=>x.KullaniciAdi == modelSignIn.KullaniciAdi);
+            if (userNameVarMi)
+            {
+                TempData["ValidateMessage"] = "This UserName is in use";
+                return RedirectToAction("Login", "Identity");
+
+            }
+            var kul = new Hasta()
+            {
+                Adi = modelSignIn.Adi,
+                Soyadi = modelSignIn.Soyadi,
+                KullaniciAdi = modelSignIn.KullaniciAdi,
+                Sifre = modelSignIn.Sifre,
+                TCNo = modelSignIn.TCNo,
+            };
+            await _context.AddAsync(kul);
+            await _context.SaveChangesAsync();
+            if (kul != null)
+            {
+                List<Claim> claims = new List<Claim>() {
+                    new Claim(ClaimTypes.NameIdentifier, modelSignIn.KullaniciAdi),
+                    new Claim(ClaimTypes.Name, kul.Id.ToString()),
+                    new Claim("Type", "Hasta")
+                };
+
+                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+                AuthenticationProperties properties = new AuthenticationProperties()
+                {
+
+                    AllowRefresh = true,
+                    IsPersistent = true
+                };
+                properties.Items["UserId"] = kul.Id.ToString();
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity), properties);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+
+
+            ViewData["ValidateMessage"] = "Kullanici Eklenemedi.";
+            return Login();
         }
     }
 }
